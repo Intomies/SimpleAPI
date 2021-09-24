c5dd5fab414df5810203e8807a0efca2933bd01d
Intomies <valtteri.nummi@gmail.com>
2021-09-24 21:29:13 +0300

    Initial Commit

---
+++ test/simpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace simpleAPI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var value = 1;
+            Assert.Equal(1, value);
+        }
+    }
+}
+
+//Link to video: 
+// https://youtu.be/SOtC1VLZKm4?t=1591
